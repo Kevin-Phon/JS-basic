36db38eff27850989cd0ec6d9279b6ebfc7cff9a
Kevin <kayaswp@gmail.com>
2022-02-12 14:24:57 +0630

    variables

---
+++ app.js
@@ -0,0 +1,11 @@
+//normal variable
+var name = "KayAs";
+console.log(name);
+
+//variable work with functions
+let namee = "Kevin";
+console.log(namee);
+
+//constant variable (can't change)
+const PI = 3.14;
+console.log(PI)
