e75dbc53117c5094c0164895ba102bf6199d3a95
Kevin <kayaswp@gmail.com>
2022-02-12 14:34:23 +0630

    normal function

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-//normal variable
-var name = "KayAs";
-console.log(name);
+// Variables
+// //normal variable
+// var name = "KayAs";
+// console.log(name);
 
-//variable work with functions
-let namee = "Kevin";
-console.log(namee);
+// //variable work with functions
+// let namee = "Kevin";
+// console.log(namee);
 
-//constant variable (can't change)
-const PI = 3.14;
-console.log(PI)
+// //constant variable (can't change)
+// const PI = 3.14;
+// console.log(PI)
+
+Function
+//Normal function
+// function getName(){
+//     console.log("KayAs")
+// }
+// getName();
+
+function getName(){
+    return("KayAs")
+}
+console.log(getName());
