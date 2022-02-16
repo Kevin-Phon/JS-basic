9a8f6a848b3fe6e27791fa1e61c7191e76a1568b
Kevin <kayaswp@gmail.com>
2022-02-16 11:59:29 +0630

    status code

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ let request = new XMLHttpRequest;
 
 request.addEventListener("readystatechange",()=>{
     //console.log(request,request.readyState);//4 readystate
-    if(request.readyState == 4){
+    if(request.readyState == 4 && request.status == 200){
         console.log(request.responseText);
+    }else if(request.status == 404){
+        console.log("Your link is unavailable!")
     }
 })
 
