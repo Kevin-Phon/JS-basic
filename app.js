6f5ad38b67099a9658f520721409ff1e9e32d06f
Kevin <kayaswp@gmail.com>
2022-02-17 13:47:43 +0630

    promises and promises chain

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-//change json format to obj and create own json file
+//promises and promises chain
 
-let getTodos = (callback) =>{
-    let request = new XMLHttpRequest;
+let getTodos = (resource) =>{
 
-    request.addEventListener("readystatechange",()=>{
-    if(request.readyState == 4 && request.status == 200){
-        let datas = JSON.parse(request.responseText); //change json to obj
-        callback(request.responseText,undefined)
-    }else if(request.status == 404){
-        callback(undefined,"Your link is unavaliable");
-    }
-})
+    return new Promise((resolve,reject) => {
+        let request = new XMLHttpRequest;
 
-    request.open("GET","kevin.json")
-    request.send();
+        request.addEventListener("readystatechange",()=>{
+            if(request.readyState == 4 && request.status == 200){
+                let datas = JSON.parse(request.responseText); //change json to obj
+                //callback(datas,undefined)
+                resolve(datas);
+            }else if(request.status == 404){
+                //callback(undefined,"Your link is unavaliable");
+                reject("Your link is unavaliable!");
+            }
+        })
+
+        request.open("GET",resource)
+        request.send();
+    })
+
 }
 
-getTodos((data,err) => {
-    if(data){
-        console.log(data);
-    }else{
-        console.log(err);
-    }
+getTodos("jsonfiles/kevin.json")
+.then((data)=>{
+    console.log(data);
+    return getTodos("jsonfiles/kayas.json")
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log(err);
 });
+    
