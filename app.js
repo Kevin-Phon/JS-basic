93aaad5b911852d3e9c1db04424201e79ed00074
Kevin <kayaswp@gmail.com>
2022-02-18 13:18:32 +0630

    fetch api

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-//promises and promises chain
+//fetch api
 
-let getTodos = (resource) =>{
-
-    return new Promise((resolve,reject) => {
-        let request = new XMLHttpRequest;
-
-        request.addEventListener("readystatechange",()=>{
-            if(request.readyState == 4 && request.status == 200){
-                let datas = JSON.parse(request.responseText); //change json to obj
-                //callback(datas,undefined)
-                resolve(datas);
-            }else if(request.status == 404){
-                //callback(undefined,"Your link is unavaliable");
-                reject("Your link is unavaliable!");
-            }
-        })
-
-        request.open("GET",resource)
-        request.send();
-    })
-
-}
-
-getTodos("jsonfiles/kevin.json")
-.then((data)=>{
-    console.log(data);
-    return getTodos("jsonfiles/kayas.json");
+fetch("jsonfiles/kevin.json")
+.then((response)=>{
+    // console.log("resolve",response.json())
+    return response.json() //promise,so use return and .then
 })
 .then((data)=>{
-    console.log(data);
-    return getTodos("jsonfiles/kyawswa.json");
-})
-.then((data)=>{
-    console.log(data);
+    console.log(data)
 })
 .catch((err)=>{
-    console.log(err);
+    console.log(err)
 });
-    
