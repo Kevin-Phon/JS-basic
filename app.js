f35a5112defa7f8381499c3ef5e2b3b8ea14c88c
Kevin <kayaswp@gmail.com>
2022-02-19 12:05:02 +0630

    throw error in async function

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+//throw error in async function
+
 let getJson =async ()=>{
-                        //promise,so use await key
-    let response = await fetch("jsonfiles/kevin.json")//will return a respose (datas),use variable to catch
+                        
+    let response = await fetch("jsonfiles/kevin.json")
+    if(response.status == 404){
+        throw new Error("Your link is not availavle!")
+    }
     let datas = await response.json();
     return datas;
 }
 
-getJson().then((datas)=>{
+getJson()
+.then((datas)=>{
     console.log(datas);
+})
+.catch((err)=>{
+    console.log(err.message)
 })
